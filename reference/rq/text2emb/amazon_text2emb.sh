460e27a313c694e5964c31807a308d2cbd268269
Mikhail Kivi <kivimike@gmail.com>
2026-01-30 19:20:10 +0500

    Stop tracking reference directory

--- reference/rq/text2emb/amazon_text2emb.sh
+++
@@ -1,4 +0,0 @@
-accelerate launch --num_processes 8 amazon_text2emb.py \
-    --dataset Industrial_and_Scientific \
-    --root ../../data/Amazon18/Industrial_and_Scientific \
-    --plm_checkpoint your_emb_model_path
